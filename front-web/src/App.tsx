1a1e855be5341d2b553e1f902ee8babca28032fc
mayumisiano <mayumi_siano10@hotmail.com>
2021-01-08 20:45:03 -0300

    Add routes and StepsHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,14 +1,10 @@
 import './App.css';
-import Navbar from './Navbar';
-import Home from './Home';
+import Routes from './Routes';
 
 function App() {
   return (
-      <div>
-      <Navbar />
-      <Home />
-      </div>
-  );
+    <Routes />
+    );
 }
 
 export default App;
